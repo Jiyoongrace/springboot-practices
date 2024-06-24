b03b0fb05e4b3c602b8dd5a776791f084c6c1f90
Jiyoongrace <giyun3632@naver.com>
2024-06-24 16:33:04 +0900

    :tada: feat: springboot test practices

---
+++ springboot-myapplication/src/test/java/com/poscodx/springboot/myapplication/SpringbootMyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscodx.springboot.myapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
