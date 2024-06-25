d495d7988afa47fa18c401956c6ac4fdc00e7b1e
Jiyoongrace <giyun3632@naver.com>
2024-06-25 10:06:55 +0900

    :art: feat: @SpringBootConfiguration

--- springboot-myapplication/src/main/java/ex02/MyApplication.java
+++ springboot-myapplication/src/main/java/ex02/MyApplication.java
@@ -1,16 +1,16 @@
-package ex01;
+package ex02;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 // 메타 어노테이션(여러 어노테이션이 합쳐져 있는 것)
 // @SpringBootApplication
 
 // @Configuration -> Spring Test Integration
 // @SpringBootApplication -> Spring Boot Test Integration
-@Configuration
+@SpringBootConfiguration
 public class MyApplication {
     @Bean
     public MyComponent myComponent() {
@@ -19,8 +19,6 @@ public class MyApplication {
 
     public static void main(String[] args) {
         try(ConfigurableApplicationContext ac = SpringApplication.run(MyApplication.class, args)){
-            MyComponent myComponent = ac.getBean(MyComponent.class);
-            System.out.println(myComponent); // ex01.MyComponent@5c48c0c0
         }
     }
 }
