b0cc6f011675ec0035f9a0c783586c7d1d1006d1
Jiyoongrace <giyun3632@naver.com>
2024-06-25 10:53:06 +0900

    :tada: feat: springboot-myweb

---
+++ springboot-myweb/src/main/java/com/poscodx/springbootmyweb/SpringbootMywebApplication.java
@@ -0,0 +1,13 @@
+package com.poscodx.springbootmyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMywebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMywebApplication.class, args);
+    }
+
+}
---
+++ springboot-myweb/src/test/java/com/poscodx/springbootmyweb/SpringbootMywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscodx.springbootmyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMywebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
